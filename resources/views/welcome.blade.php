39c4b18c8ba7fcad17f1a59f786eda47c6be1b1f
HasbianNur <kitaraja600@gmail.com>
2023-02-14 09:03:28 +0800

    (Ubah Title Website)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel Dengan Livewire</title>
+    <title>Laravel | HasbianNur</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
@@ -13,7 +13,8 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        <h2 class="text-center text-primary">Laravel Dengan Livewire</h2>
+                        <h2 class="text-center text-primary">Laravel CRUD</h2>
+                        <p class="text-center">App CRUD yang menggunakan Livewire</p>
                     </div>
                     <div class="card-body">
                         @if (session()->has('message'))
